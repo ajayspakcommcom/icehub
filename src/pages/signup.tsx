44b914fc5caa79a32c265ec7f443ba259f80d6a8
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-03-15 15:25:45 +0530

    latest changes

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,25 +2,47 @@ import * as React from 'react';
 import dynamic from 'next/dynamic';
 import Image from "next/image";
 import BannerCarouselImage from "@/models/BannerCarouselImage";
+import { getHomeList } from "@/services/home-banner";
 
 const BannerCarousel = dynamic(() => import('@/components/carousel/banner-carousel'));
 const FormHeading = dynamic(() => import('@/components/form-heading/form-heading'));
 const FormFooter = dynamic(() => import('@/components/form-footer/form-footer'));
 const SignupForm = dynamic(() => import('@/components/signup/SignupForm'), { ssr: false });
 
-const images: BannerCarouselImage[] = [
-  { id: 1, imageUrl: '1.png' },
-  { id: 2, imageUrl: '1.png' },
-  { id: 3, imageUrl: '1.png' },
-];
+const images: BannerCarouselImage[] = [];
 
 export default function ComboBox() {
+
+  const [bannerCarousels, setBannerCarousels] = React.useState<BannerCarouselImage[]>([]);
+
+  React.useEffect(() => {
+
+    const fetchHomeDataList = async () => {
+
+      try {
+
+        const formData = new FormData();
+        formData.append('type', 'LIST');
+        const response = await getHomeList(formData, localStorage.getItem('token')!);
+        const responseData = response.data.data;
+        setBannerCarousels(responseData);
+      } catch (error: any) {
+        console.log('Error : ', error);
+      };
+      return () => console.log('');
+    }
+
+    fetchHomeDataList();
+
+
+  }, []);
+
   return (
     <>
 
       <div className="login-wrapper">
         <div>
-          <BannerCarousel images={images} />
+          <BannerCarousel images={bannerCarousels} />
         </div>
         <div>
           <div className="login-form-wrapper">
